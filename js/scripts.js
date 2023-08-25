c2b162523d1822f5ff9f4523791642f3c117b981
Seth Gonzales <sethgonzales157@gmail.com>
2023-08-25 13:45:52 -0700

    give order response with lowercased values

--- js/scripts.js
+++ js/scripts.js
@@ -31,6 +31,22 @@ Pizza.prototype.totalCostCalc = function () {
   return "$" + this.totalCost;
 };
 
+Pizza.prototype.toppingsToString = function () {
+  let toppingsString = "";
+  for (let i = 0; i < toppingsArray.length; i++) {
+    if (toppingsArray.length === 1) {
+      toppingsString = toppingsArray[i];
+    } else if (toppingsArray.length === 2) {
+      toppingsString = toppingsArray[0] + " and " + toppingsArray[1];
+    } else if (i === toppingsArray.length - 1) {
+      toppingsString += "and " + toppingsArray[i];
+    } else {
+      toppingsString += toppingsArray[i] + ", ";
+    }
+  }
+  return toppingsString;
+};
+
 //UI Logic
 
 function handleFormSubmission(event) {
@@ -40,30 +56,18 @@ function handleFormSubmission(event) {
   const pizzaSize = document.querySelector("input[name=size-order]:checked").value;
   const pizzaToppings = document.querySelectorAll("input[type=checkbox]:checked");
   const toppingsArray = []
-
   pizzaToppings.forEach(function (checkbox) {
     toppingsArray.push(checkbox.value);
   });
 
   const myPizza = new Pizza(customerName, toppingsArray, pizzaSize);
 
+  let firstName
+
   document.getElementById("show-cost").removeAttribute("class");
-  document.getElementById("size").innerText = myPizza.size;
-  
-
-  let toppingsString = "";
-  for (let i=0; i<toppingsArray.length; i++) {
-    if (toppingsArray.length === 2) {
-      toppingsString = toppingsArray[i] + " ";
-  } else if (i === toppingsArray.length - 1) {
-      toppingsString = toppingsString + "and " + toppingsArray[i];
-  } else {
-    toppingsString = toppingsArray[i] + ", " ;
-  }
-  }; 
-
-  document.getElementById("toppings").innerText = toppingsString;
-
+  document.getElementById("name").innerText = myPizza.name.charAt(0).toUpperCase();
+  document.getElementById("size").innerText = myPizza.size.toLowerCase();
+  document.getElementById("toppings").innerText = myPizza.toppingsToString().toLowerCase();
   document.getElementById("cost-value").innerText = myPizza.totalCostCalc();
 }
 
